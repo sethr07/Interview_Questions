7ccf2e366e39c923dec5d6cafd37f4c9aea02b58
Rahul Seth <57781892+sethr07@users.noreply.github.com>
2023-02-06 02:21:01 +0000

    Create sol1.py

---
+++ 1470. Shuffle the Array/sol1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+
+        res = []
+
+        for i in range(0,n):
+            res.append(nums[i])
+            res.append(nums[i+n])
+        
+        
+        return res
